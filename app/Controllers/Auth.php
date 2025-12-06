2a929ca08cb7141075cae088d09f4f43d3319865
acell123 <cellostt132@gmail.com>
2025-12-06 16:47:56 +0700

    tes

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -67,17 +67,14 @@ class Auth extends BaseController
         $password_hash = $this->request->getPost('password_hash');
         $confirm  = $this->request->getPost('confirm');
 
-        // Validasi password dan confirm
         if ($password_hash !== $confirm) {
             return redirect()->back()->with('error', 'Password tidak sama!');
         }
 
-        // Cek apakah email atau full_name sudah ada
         if ($userModel->where('email', $email)->orWhere('full_name', $full_name)->first()) {
             return redirect()->back()->with('error', 'Email atau full_name sudah digunakan!');
         }
 
-        // Simpan user (PASSWORD HASH bukan MD5!!)
         $userModel->insert([
             'full_name' => $full_name,
             'email'    => $email,
